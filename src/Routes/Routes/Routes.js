d3dc8db63f1539fa43a3c4e19a7e865698e6f7bd
ShahanazAhmedNishi <asahanaz207@gmail.com>
2022-10-26 01:27:42 +0600

    data load from server

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -16,6 +16,8 @@ export const routes = createBrowserRouter([
       {
         path: "/category/:id",
         element: <Category></Category>,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/category/${params.id}`),
       },
       {
         path: "/courses/:id",
