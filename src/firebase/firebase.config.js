ce6556e4f00749bd1d383d19947b3bcd9ed2c83c
ShahanazAhmedNishi <asahanaz207@gmail.com>
2022-10-26 00:12:40 +0600

    route setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkhcY6LS0OUpC8BPpKKv6B6lxPLHmKhqQ",
+  authDomain: "edutech-849e4.firebaseapp.com",
+  projectId: "edutech-849e4",
+  storageBucket: "edutech-849e4.appspot.com",
+  messagingSenderId: "157395373934",
+  appId: "1:157395373934:web:2a2d3db536c9a1c9dbfb7f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
