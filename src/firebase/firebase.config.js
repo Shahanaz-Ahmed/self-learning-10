438c7b0ba5cbaf5f958a42f57495d10a362c4744
ShahanazAhmedNishi <asahanaz207@gmail.com>
2022-10-27 00:58:39 +0600

    added environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCkhcY6LS0OUpC8BPpKKv6B6lxPLHmKhqQ",
-  authDomain: "edutech-849e4.firebaseapp.com",
-  projectId: "edutech-849e4",
-  storageBucket: "edutech-849e4.appspot.com",
-  messagingSenderId: "157395373934",
-  appId: "1:157395373934:web:2a2d3db536c9a1c9dbfb7f",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
